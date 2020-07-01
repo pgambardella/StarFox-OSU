fb2795feabb3ce4c9208bcd87e3e0bbaaa815534
Paolo Gambardella <pgambardella@generagames.com>
2020-07-01 12:19:41 +0200

    added change lane functionality (bugged)

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public event UnityAction<string> onUiInteraction;
+    public void UiInteraction(string uiName)
+    {
+        if (onUiInteraction != null)
+        {
+            onUiInteraction(uiName);
+        }
+    }
+
+    public event UnityAction onShowSwitchPathUI;
+    public void ShowSwitchPathUI()
+    {
+        if (onShowSwitchPathUI != null)
+        {
+            onShowSwitchPathUI();
+        }
+    }
+
+    public event UnityAction onHideSwitchPathUI;
+    public void HideSwitchPathUI()
+    {
+        if (onHideSwitchPathUI != null)
+        {
+            onHideSwitchPathUI();
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchPathManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPathManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        EventManager.instance.ShowSwitchPathUI();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EventManager.instance.HideSwitchPathUI();
+    }
+}
