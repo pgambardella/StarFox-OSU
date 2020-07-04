51905291770d285bea1882293dfda3de3f3e299f
Paolo Gambardella <pgambardella@generagames.com>
2020-07-04 20:39:48 +0200

    added gun and 2 kind of enemies

---
+++ Assets/Scripts/EnemyActivator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActivator : MonoBehaviour
+{
+    public GameObject m_enemyObj;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_enemyObj != null)
+        {
+            if (m_enemyObj.activeSelf)
+            {
+                Debug.LogError("the enemy " + m_enemyObj.name + " was already activated");
+            }
+            m_enemyObj.SetActive(true);
+        } else
+        {
+            Debug.LogError("you have to associate an Enemy to the EnemyActivator component of " + gameObject.name);
+        }
+    }
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_bulletPrefab;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (m_bulletPrefab)
+            {
+                Instantiate(m_bulletPrefab, transform.position, transform.rotation * m_bulletPrefab.transform.rotation);
+            } else
+            {
+                Debug.LogError("you forgot to put the bullet prefab in the weapon: " + gameObject.name);
+            }
+        }
+    }
+}
